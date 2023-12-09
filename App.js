f188f838e9a98ed3f48b87c250dd03f8efbb3b0e
JeanProvost <jprov310@gmail.com>
2023-12-08 23:51:17 -0700

    Cleaning up imports in App.js

--- App.js
+++ App.js
@@ -1,14 +1,4 @@
 import React from "react";
-import { Text, View } from "react-native";
-import WelcomeScreen from "./app/screens/WelcomeScreen";
-import ViewImageScreen from "./app/screens/ViewImageScreen";
-import {MaterialCommunityIcons} from '@expo/vector-icons';
-import colors from './app/config/colors'
-import AppText from "./app/components/AppText";
-import Card from "./app/components/Card";
-import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
-import MessagesScreen from "./app/screens/MessagesScreen";
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import AccountScreen from "./app/screens/AccountScreen";
 
 export default function App() {
